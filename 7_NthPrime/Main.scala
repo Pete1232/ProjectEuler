061205c0543cc263f1794d9a462f652d0ed10e2d
peter.newman <peter.newman@digital.hmrc.gov.uk>
2016-03-12 15:04:45 +0000

    Problem 7

---
+++ 7_NthPrime/Main.scala
@@ -0,0 +1,26 @@
+object Main extends App{
+  println(nthPrime(target = args(0).toInt))
+
+  def nthPrime(n: Int = 1, number: Int = 2, target: Int): Int ={
+    if(isPrime(number = number)){
+      if(n == target)
+        number
+      else
+        nthPrime(n+1, number+1, target)
+    }
+    else
+      nthPrime(n, number+1, target)
+  }
+
+  def isPrime(number: Int) = {
+    def checkPrime(divisor: Int = 2, number: Int): Boolean = {
+      if(divisor == number)
+        true
+      else if(number%divisor != 0)
+        checkPrime(divisor+1, number)
+      else
+        false
+    }
+    checkPrime(number = number)
+  }
+}
