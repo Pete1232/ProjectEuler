a1429114f4400da081216d0bb5d0d64eb637cf20
Pete1232 <pete1232@gmail.com>
2018-05-12 23:48:51 +0100

    Quick change to Eq instance

--- src/test/scala/numeric/LongNumberLawsSpec.scala
+++ src/test/scala/numeric/LongNumberLawsSpec.scala
@@ -10,7 +10,7 @@ class LongNumberLawsSpec extends CatsSuite {
 
   implicit val arbitraryLongNumber: Arbitrary[LongNumber] = Arbitrary(Arbitrary.arbBigInt.arbitrary.map(LongNumber.fromBigInt))
 
-  implicit def eqTree: Eq[LongNumber] = Eq.instance(_ == _)
+  implicit def eqTree: Eq[LongNumber] = Eq.fromUniversalEquals
 
   checkAll("LongNumber.MonoidLaws", MonoidTests[LongNumber].monoid)
 }
